f7e7b52f9833eebc2eb5b168c7ccd0f48c9b6aa9
dijksterhuis <mrobeson@dundee.ac.uk>
2017-11-27 23:41:22 +0000

    alpine so ash not bash

--- 2-discogs-datastorage/2-xmls2mongo/docker_run.sh
+++ 2-discogs-datastorage/2-xmls2mongo/docker_run.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
     --network discogs-mongo \
     --name discogs-pymongoimport \
     dijksterhuis/discogs-pymongoimport:0.1.0 \
-    /bin/bash -c 'find /home/xmls -name \*.xml -exec /home/convert_xmls_2.py {} \;'
+    /bin/ash -c 'find /home/xmls -name \*.xml -exec /home/convert_xmls_2.py {} \;'
 #     /bin/ash -c '/home/convert_xmls.py /home/xmls/discogs_20170901_masters.xml --verbose'
