e4fd7f6453d6cf9f1b152c5dc5791ffddc4f9859
dijksterhuis <mrobeson@dundee.ac.uk>
2017-11-27 23:39:27 +0000

    update container version number

--- 2-discogs-datastorage/2-xmls2mongo/docker_run.sh
+++ 2-discogs-datastorage/2-xmls2mongo/docker_run.sh
@@ -4,6 +4,6 @@ docker run -it --rm \
     --link mongo-discogs \
     --network mongo-json-access \
     --name discogs-pymongoimport \
-    dijksterhuis/discogs-pymongoimport:0.0.3 \
+    dijksterhuis/discogs-pymongoimport:0.1.0 \
     /bin/bash -c 'find /home/xmls -name \*.xml -exec /home/convert_xmls_2.py {} \;'
 #     /bin/ash -c '/home/convert_xmls.py /home/xmls/discogs_20170901_masters.xml --verbose'
