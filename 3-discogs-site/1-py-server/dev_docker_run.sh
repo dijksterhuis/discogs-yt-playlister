35b92bc9be70e63af960b61334d937e015177eac
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 20:53:38 +0000

    update

--- 3-discogs-site/1-py-server/dev_docker_run.sh
+++ 3-discogs-site/1-py-server/dev_docker_run.sh
@@ -5,13 +5,15 @@ image_tag="dev"
 container_name="pyserving-test"
 networks='discogs-metadata-stores discogs-redis-site-queries'
 
-docker run -d --rm -p 80:5000 -p 4040:4040 \
+docker run \
+    -di --restart=always \
+    -p 80:5000 -p 4040:4040 \
     -v $pth:/home/site \
     -w /home \
     --name $container_name \
-        $image_name":"$image_tag
+    $image_name":"$image_tag
 
 for net in $networks ; do docker network connect $net $container_name ; done
 
-docker exec -it $container_name /bin/ash -c "/home/site/main.py"
+docker exec -it $container_name /bin/ash -c "python /home/site/main.py"
 docker stop $container_name; docker rm $container_name
