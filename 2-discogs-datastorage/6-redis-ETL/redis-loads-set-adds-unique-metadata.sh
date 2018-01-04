507be2ae0bdf56f1b29d50afbb5d04e9dc0c1dba
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 21:15:37 +0000

    daemon process and query form changes

--- 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
@@ -5,7 +5,7 @@ do \
 echo 'Running for tag '$tag ; \
 image='dijksterhuis/redis-database-inserts:modularised-0.1' ;\
 container_name='redis-loads-unique-'$tag ;\
-container_command='./redis-load-set-adder.py' ;\
+container_command='./redis-load-set-adder.py ; exit' ;\
 container_args='simple_set masters redis-metadata-unique-'$tag' '$tag' masters_id' ;\
 networks='discogs-metadata-stores discogs-mongo' ;\
 echo $container_name ; echo $image ;\
@@ -15,6 +15,6 @@ echo $container_name ; echo $image ;\
             --name $container_name \
             $image ; \
          for network in $networks; do docker network connect $network $container_name ; echo "connected to "$network ; done ; \
-        docker exec -i $container_name $container_command $container_args ; \
-        docker stop $container_name ; docker rm $container_name \
+        docker exec -di $container_name $container_command $container_args ; \
+        #docker stop $container_name ; docker rm $container_name \
 ; done
