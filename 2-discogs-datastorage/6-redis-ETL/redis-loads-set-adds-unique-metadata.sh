fab4ace63f9a77ded52765f45ad114d542d05b28
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 05:25:57 +0000

    asfas

--- 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
@@ -2,12 +2,12 @@
 
 for tag in genre style year; \
 do \
-    echo 'Running for tag' $tag ; \
-    image='dijksterhuis/redis-database-inserts:modularised-0.1' \
-    container_name='redis-loads-unique-'$tag \
-    container_command='./redis-load-set-adder.py' \
-    container_args='simple_set masters redis-metadata-unique-'$tag' '$tag' masters_id' \
-    networks='discogs-metadata-stores discogs-mongo' \
+echo 'Running for tag '$tag ; \
+image='dijksterhuis/redis-database-inserts:modularised-0.1' \
+container_name='redis-loads-unique-'$tag \
+container_command='./redis-load-set-adder.py' \
+container_args='simple_set masters redis-metadata-unique-'$tag' '$tag' masters_id' \
+networks='discogs-metadata-stores discogs-mongo' \
         echo $container_name ;\
         docker run -di \
             -w /home \
