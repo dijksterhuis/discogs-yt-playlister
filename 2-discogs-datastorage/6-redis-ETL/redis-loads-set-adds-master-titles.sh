b9483a3f67f379bdb2f06a60aa689fee9078fc2d
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 03:55:23 +0000

    set based redis load docker run scripts

---
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-master-titles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+image='dijksterhuis/redis-database-inserts:modularised-0.1'
+container_name='redis-loads-master-titles'
+container_command='./redis-load-set-adder.py'
+container_args='masters redis-masters-ids release_title masters_id'
+networks='discogs-redis-site-queries discogs-mongo'
+
+docker run -di \
+    -w /home \
+    -v metadata-extraction-logs:/logging \
+    --name $container_name \
+    $image
+
+for network in $networks; do docker network connect $network $container_name ; echo "connected to "$network ; done
+
+docker exec -d $container_name $container_command $container_args
+
+docker stop $container_name ; docker rm $container_name
---
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-video-ids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+image='dijksterhuis/redis-database-inserts:modularised-0.1'
+container_name='redis-loads-video-urls'
+container_command='./redis-load-set-adder.py'
+container_args='masters redis-video-id-urls masters_id video_url'
+networks='discogs-redis-site-queries discogs-mongo'
+
+docker run -di \
+    -w /home \
+    -v metadata-extraction-logs:/logging \
+    --name $container_name \
+    $image
+
+for network in $networks; do docker network connect $network $container_name ; echo "connected to "$network ; done
+
+docker exec -d $container_name $container_command $container_args
+
+docker stop $container_name ; docker rm $container_name
