cf88bb0ada0153b3fb3bdcda64cd0b4b36671acf
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 20:45:44 +0000

    docker run site update

--- 3-discogs-site/1-py-server/dev_docker_run.sh
+++ 3-discogs-site/1-py-server/dev_docker_run.sh
@@ -1,8 +1,17 @@
-pth="/home/dijksterhuis/Documents/discogs-yt-playlister/3-discogs-site/1-py-server"
+pth="/home/dijksterhuis/Documents/discogs-yt-playlister/3-discogs-site/1-py-server/flask-site"
+
+image_name="dijksterhuis/discogs-flask-server"
+image_tag="dev"
+container_name="pyserving-test"
+networks="discogs-metadata-stores discogs-redis-site-queries"
 
 docker run -it --rm -p 80:5000 -p 4040:4040 \
     -v $pth:/home/site \
     -w /home \
-    --network discogs-redis-site-queries \
-    --name pyserving-test \
-    dijksterhuis/discogs-flask-server:dev /bin/ash
+    --name $container_name \
+        $image_name":"$image_tag
+
+for net in networks ; do docker network connect $net $container_name ; done
+
+docker exec -it $container_name /bin/ash -c "/home/site/main.py"
+docker stop $container_name; docker rm $container_name
