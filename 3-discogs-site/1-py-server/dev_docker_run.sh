1969f6ae1dc1add29045279cb8f711c6ec0f91fd
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 20:48:01 +0000

    var declaration error

--- 3-discogs-site/1-py-server/dev_docker_run.sh
+++ 3-discogs-site/1-py-server/dev_docker_run.sh
@@ -11,7 +11,7 @@ docker run -d --rm -p 80:5000 -p 4040:4040 \
     --name $container_name \
         $image_name":"$image_tag
 
-for net in networks ; do docker network connect $net $container_name ; done
+for net in $networks ; do docker network connect $net $container_name ; done
 
 docker exec -it $container_name /bin/ash -c "/home/site/main.py"
 docker stop $container_name; docker rm $container_name
