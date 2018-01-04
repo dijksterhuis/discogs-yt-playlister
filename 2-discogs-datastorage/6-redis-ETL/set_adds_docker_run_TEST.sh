e3f9e6b2fd9cdba7a44b691028e5251f01842dc9
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 02:59:50 +0000

    network fun times

--- 2-discogs-datastorage/6-redis-ETL/set_adds_docker_run_TEST.sh
+++ 2-discogs-datastorage/6-redis-ETL/set_adds_docker_run_TEST.sh
@@ -2,10 +2,17 @@
 
 image='dijksterhuis/redis-database-inserts:modularised-0.1'
 container_name='discogs-testing'
-container_command='masters redis-masters-ids release_title masters_id'
-docker_network='discogs-redis-site-queries'
+container_command='/redis-load-set-adder.py masters redis-masters-ids release_title masters_id'
+networks='discogs-redis-site-queries discogs- mongo'
+
 docker run -di \
+    -w /home \
     -v metadata-extraction-logs:/logging \
     --name $container_name \
-    --network $docker_network
-    $image container_command
+    $image
+
+for network in $networks; do docker network connect $network $container_name ; echo "connected to "$network ; done
+
+docker exec -i $container_name /bin/ash -c $container_command
+
+docker stop $container_name ; docker rm $container_name
