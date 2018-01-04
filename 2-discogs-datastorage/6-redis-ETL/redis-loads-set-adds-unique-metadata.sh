f7cb3bf88d47515510a0feb6e94bec74a06ab438
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-04 21:27:25 +0000

    fixes to python etl

--- 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-unique-metadata.sh
@@ -6,7 +6,7 @@ echo 'Running for tag '$tag ; \
 image='dijksterhuis/redis-database-inserts:modularised-0.1' ;\
 container_name='redis-loads-unique-'$tag ;\
 container_command='./redis-load-set-adder.py ; exit' ;\
-container_args='simple_set masters redis-metadata-unique-'$tag' '$tag' masters_id' ;\
+container_args='simple_set masters redis-metadata-unique-'$tag' '$tag's masters_id' ;\
 networks='discogs-metadata-stores discogs-mongo' ;\
 echo $container_name ; echo $image ;\
         docker run -di \
