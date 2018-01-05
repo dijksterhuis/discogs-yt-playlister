62088e51bcfbe2a0e4c5cbb27ac31df78232f628
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-05 11:12:51 +0000

    test changes youtube playlist get

--- 3-discogs-site/1-py-server/flask-site/youtube_playlist_gen.py
+++ 3-discogs-site/1-py-server/flask-site/youtube_playlist_gen.py
@@ -1,7 +1,7 @@
 # This code sample creates a private playlist in the authorizing user's
 # YouTube channel.
 # Usage:
-#   python playlist_updates.py --title=<TITLE> --description=<DESCRIPTION>
+# 	python playlist_updates.py --title=<TITLE> --description=<DESCRIPTION> --video_ids=<VIDEO_ID>
 
 import argparse
 import os
@@ -21,10 +21,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 # {{ https://cloud.google.com/console }}.
 # Please ensure that you have enabled the YouTube Data API for your project.
 # For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
+# 	https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-    
+# 	https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+
 CLIENT_SECRETS_FILE = '/home/client_secrets.json'
 
 # This OAuth 2.0 access scope allows for full read/write access to the
@@ -32,46 +32,110 @@ CLIENT_SECRETS_FILE = '/home/client_secrets.json'
 SCOPES = ['https://www.googleapis.com/auth/youtube']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
-    
+
 # Authorize the request and store authorization credentials.
 def get_authenticated_service():
-  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_console()
-  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
-    
-def add_playlist(youtube, args):
-  
-  body = dict(
-    snippet=dict(
-      title=args.title,
-      description=args.description
-    ),
-    status=dict(
-      privacyStatus='private'
-    ) 
-  ) 
-    
-  playlists_insert_response = youtube.playlists().insert(
-    part='snippet,status',
-    body=body
-  ).execute()
+	flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+	credentials = flow.run_console()
+	return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
 
-  print('New playlist ID: %s' % playlists_insert_response['id'])
-  
+def print_response(response):
+	print(response)
+
+# Build a resource based on a list of properties given as key-value pairs.
+# Leave properties with empty values out of the inserted resource.
+def build_resource(properties):
+	resource = {}
+	for p in properties:
+		# Given a key like "snippet.title", split into "snippet" and "title", where
+		# "snippet" will be an object and "title" will be a property in that object.
+		prop_array = p.split('.')
+		ref = resource
+		for pa in range(0, len(prop_array)):
+			is_array = False
+			key = prop_array[pa]
+			
+			# For properties that have array values, convert a name like
+			# "snippet.tags[]" to snippet.tags, and set a flag to handle
+			# the value as an array.
+			if key[-2:] == '[]':
+				key = key[0:len(key)-2:]
+				is_array = True
+			
+			if pa == (len(prop_array) - 1):
+				# Leave properties without values out of inserted resource.
+				if properties[p]:
+					if is_array:
+						ref[key] = properties[p].split(',')
+					else:
+						ref[key] = properties[p]
+			elif key not in ref:
+				# For example, the property is "snippet.title", but the resource does
+				# not yet have a "snippet" object. Create the snippet object here.
+				# Setting "ref = ref[key]" means that in the next time through the
+				# "for pa in range ..." loop, we will be setting a property in the
+				# resource's "snippet" object.
+				ref[key] = {}
+				ref = ref[key]
+			else:
+				# For example, the property is "snippet.description", and the resource
+				# already has a "snippet" object.
+				ref = ref[key]
+	return resource
+
+# Remove keyword arguments that are not set
+def remove_empty_kwargs(**kwargs):
+	good_kwargs = {}
+	if kwargs is not None:
+		for key, value in kwargs.iteritems():
+			if value:
+				good_kwargs[key] = value
+	return good_kwargs
+
+def playlist_items_insert(client, properties, **kwargs):
+	# See full sample for function
+	resource = build_resource(properties)
+	# See full sample for function
+	kwargs = remove_empty_kwargs(**kwargs)
+	response = client.playlistItems().insert(body=resource,**kwargs).execute()
+	return print_response(response)
+
+def add_playlist(youtube, pl_title, pl_description):
+	body = dict( snippet=dict( title=pl_title, description=pl_description ), status=dict( privacyStatus='private' ) ) 
+	playlists_insert_response = youtube.playlists().insert( part='snippet,status', body=body ).execute()
+	print('New playlist ID: %s' % playlists_insert_response['id'])
+	return playlists_insert_response
+
+def create_playlist(pl_title,pl_description):
+	youtube = get_authenticated_service()
+	try:
+		return add_playlist(youtube, pl_title,pl_description)
+	except HttpError as e:
+		print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+		exit(e.resp.status)
+
+def insert_videos(playlist_insert_response,video_ids):
+	pl_id = playlists_insert_response['id']
+	youtube = get_authenticated_service()
+	if len(video_ids) > 0:
+		#for video_id in video_ids:
+		if 'http' in video_id or 'youtube.com' in video_id:
+			video_id = video_id.replace('https://youtube.com/watch?v=','')
+		playlist_items_insert(\
+					youtube, { 'snippet.playlistId': pl_id, 'snippet.resourceId.kind': 'youtube#video' \
+								, 'snippet.resourceId.videoId[]': video_ids, 'snippet.position': '' \
+							}, part='snippet', onBehalfOfContentOwner='' \
+					)
+
 if __name__ == '__main__':
-           
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--title',
-      default='Test Playlist',
-      help='The title of the new playlist.')
-  parser.add_argument('--description',
-      default='A private playlist created with the YouTube Data API.',
-      help='The description of the new playlist.')
-    
-  args = parser.parse_args()
-    
-  youtube = get_authenticated_service()
-  try:
-    add_playlist(youtube, args)
-  except HttpError as e:
-    print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--title', type=str, nargs=1 \
+										, default='Test Playlist'\
+										, help='The title of the new playlist.')
+	parser.add_argument('--description', type=str, nargs=1 \
+										, default='A private playlist created with the YouTube Data API.' \
+										, help='The description of the new playlist.')
+	parser.add_argument('--video_ids', type=str, nargs='+', dest = 'video_id' \
+										,help='Video ID(s) to add to the playlist.')
+	args = parser.parse_args()
+	insert_videos( create_playlist(args.title,args.description), args.video_ids)
