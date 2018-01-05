fa09235e685b8f92b0e6ac4d9c5db34034bca4dd
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-05 01:53:53 +0000

    sdgkj

--- 3-discogs-site/1-py-server/flask-site/youtube_playlist_gen.py
+++ 3-discogs-site/1-py-server/flask-site/youtube_playlist_gen.py
@@ -49,7 +49,7 @@ flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
   message=MISSING_CLIENT_SECRETS_MESSAGE,
   scope=YOUTUBE_READ_WRITE_SCOPE)
 
-storage = Storage("oauth2.json" % sys.argv[0])
+storage = Storage("oauth2.json")
 credentials = storage.get()
 
 if credentials is None or credentials.invalid:
