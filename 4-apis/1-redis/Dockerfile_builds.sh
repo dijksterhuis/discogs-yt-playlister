1e457b82b866572f58daa8ab711e02ba3f42d6c0
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-07 19:01:40 +0000

    semicolon typo

--- 4-apis/1-redis/Dockerfile_builds.sh
+++ 4-apis/1-redis/Dockerfile_builds.sh
@@ -2,13 +2,14 @@
 
 # build and run all images / run scripts in lower directories
 
+
 for file in * ; \
     do \
         if [ -d $file ] ; \
             then \
-                git pull ; \
+                echo 'Building and running: '$file ; \
                 docker-build-contextual './'$file ; \
                 chmod u+x './'$file'/docker_run.sh' ; \
-                './'$file'/docker_run.sh' \
+                './'$file'/docker_run.sh' ; \
         fi ; \
     done
