cc183c9fb6eb6dfca9d61a3c75b55567982bb16f
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-07 19:16:18 +0000

    update

--- 4-apis/1-redis/Dockerfile_builds.sh
+++ 4-apis/1-redis/Dockerfile_builds.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # build and run all images / run scripts in lower directories
-
+cwd=$(pwd)
 pth=/home/dijksterhuis/Documents/discogs-yt-playlister/4-apis/1-redis
-for file in $pth/* ; \
+cd $pth
+
+for file in * ; \
     do \
         if [ -d $file ] ; \
             then \
-                mv -v $pth'/DEV_api_build_funx.py' $file'/api_build_funx.py' ; \
+                mv -v 'DEV_api_build_funx.py' $file'/api_build_funx.py' ; \
                 echo 'Building and running: '$file ; \
                 docker-build-contextual $file ; \
                 chmod u+x $file'/docker_run.sh' ; \
-                $file'/docker_run.sh' ; \
+                ./$file'/docker_run.sh' ; \
         fi ; \
     done
+    
+cd $cwd
