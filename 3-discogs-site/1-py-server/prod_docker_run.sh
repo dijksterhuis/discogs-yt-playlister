326153a01d8fd9b59bc17440a1e249b6debb4485
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-07 20:21:20 +0000

    update

---
+++ 3-discogs-site/1-py-server/prod_docker_run.sh
@@ -0,0 +1,19 @@
+image_name="dijksterhuis/discogs-flask-server"
+image_tag="dev"
+container_name="pyserving-test"
+networks='discogs-get-apis'
+numb_nets=$(echo $container_names | wc -w)
+
+docker run \
+    -di --restart=always \
+    -p 80:80 \
+    -w /home \
+    --name $container_name \
+    $image_name":"$image_tag
+
+if [ numb_nets > 1 ]; then \
+    for net in $networks ; do docker network connect $net $container_name ; done ; \
+else \
+    docker network connect $networks $container_name ; \
+fi
+
