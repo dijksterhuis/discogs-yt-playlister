ff6955813080a6b281f0ef788f5fbe28f1b8d7cc
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-07 19:12:08 +0000

    update for absolute instead of relative paths

--- 4-apis/1-redis/Dockerfile_builds.sh
+++ 4-apis/1-redis/Dockerfile_builds.sh
@@ -2,15 +2,15 @@
 
 # build and run all images / run scripts in lower directories
 
-
-for file in * ; \
+pth=/home/dijksterhuis/Documents/discogs-yt-playlister/4-apis/1-redis
+for file in $pth/* ; \
     do \
         if [ -d $file ] ; \
             then \
-                mv -v './DEV_api_build_funx.py' './'$file'/api_build_funx.py' ; \
+                mv -v $pth'/DEV_api_build_funx.py' $file'/api_build_funx.py' ; \
                 echo 'Building and running: '$file ; \
-                docker-build-contextual './'$file ; \
-                chmod u+x './'$file'/docker_run.sh' ; \
-                './'$file'/docker_run.sh' ; \
+                docker-build-contextual $file ; \
+                chmod u+x $file'/docker_run.sh' ; \
+                $file'/docker_run.sh' ; \
         fi ; \
     done
