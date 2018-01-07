6b8321ae2968f24477eb8f0723fc94e4a45aa597
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-07 22:48:48 +0000

    stop/rm logic for pyserving

--- 3-discogs-site/1-py-server/prod_docker_run.sh
+++ 3-discogs-site/1-py-server/prod_docker_run.sh
@@ -5,6 +5,9 @@ api_net='discogs-get-apis'
 redis_net='discogs-redis-site-queries'
 numb_nets=$(echo $container_names | wc -w)
 
+docker stop $container_name
+docker rm $container_name
+
 docker run \
     -di --restart=always \
     -p 70:80 \
