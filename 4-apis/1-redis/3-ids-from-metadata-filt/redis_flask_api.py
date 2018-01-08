802ac7c4341101b78c86267a4b953c1c252477b6
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-08 01:59:20 +0000

    update

--- 4-apis/1-redis/3-ids-from-metadata-filt/redis_flask_api.py
+++ 4-apis/1-redis/3-ids-from-metadata-filt/redis_flask_api.py
@@ -36,5 +36,4 @@ def get_ids(args):
 	return result
 
 if __name__ == '__main__':
-	#app.run(host='0.0.0.0',port=80,debug=True)
-	app.run(host='0.0.0.0',port=80,debug=True)
+	app.run(host='0.0.0.0',port=80,debug=False)
