d8f958e85ebf5e011075e996902df8ac43ea544b
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-08 01:09:18 +0000

    testing

--- 4-apis/1-redis/3-ids-from-metadata-filt/redis_flask_api.py
+++ 4-apis/1-redis/3-ids-from-metadata-filt/redis_flask_api.py
@@ -8,7 +8,7 @@ from webargs.flaskparser import use_args
 
 #### CUSTOM BUILD FUNCTION IMPORTS:
 
-from api_build_funx import timer, get_metadata_ids, make_json_resp,  METADATA_ID_ARGS
+from api_build_funx import timer, make_json_resp,  NAME_ARGS
 
 """
 https://flask-restful.readthedocs.io/en/0.3.5/index.html
@@ -27,13 +27,13 @@ def alive():
 	return make_json_resp( {'status': 'OK'} , 200 )
 
 @app.route('/ids_from_metadata', methods=['GET'])
-@use_args(METADATA_ID_ARGS,locations=('json', 'form'))
+@use_args(NAME_ARGS,locations=('json', 'form'))
 def metadata_ids(args):
 	req_time = timer()
 	print(args)
 	#result = get_metadata_ids( args['metadata_filter_dict'] )
 	print('request time taken', req_time.time_taken() )
-	return make_json_resp( args['metadata_filter_dict'] , 200 )
+	return make_json_resp( args , 200 )
 
 if __name__ == '__main__':
 	#app.run(host='0.0.0.0',port=80,debug=True)
