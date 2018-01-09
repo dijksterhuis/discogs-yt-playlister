374c67c31aeac2fa69fdbc561590d65ee616a512
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-09 07:42:30 +0000

    paofj

--- 3-discogs-site/2-nginx/docker_run.sh
+++ 3-discogs-site/2-nginx/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -di -p 127.0.0.1:80:80 -p 127.0.0.1:443:443 --network discogs-web-serve --name discogs-nginx discogs-nginx-proxy:dev
+docker run -di -p 127.0.0.1:80:80 -p 127.0.0.1:443:443 --network discogs-web-serve --name discogs-nginx dijksterhuis/discogs-nginx-proxy
