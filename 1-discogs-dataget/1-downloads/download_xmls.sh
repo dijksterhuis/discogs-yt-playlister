d3c405bc04f39b4500ee5fe363fc9f7c6d6238dd
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-10 13:16:50 +0000

    downloads logic changes

--- 1-discogs-dataget/1-downloads/download_xmls.sh
+++ 1-discogs-dataget/1-downloads/download_xmls.sh
@@ -10,28 +10,29 @@ IFS="$(printf '\n\t')"
 
 
 ##### 1
-# Variable declarations - working path, current dates
+# - Variable declarations - working path, current dates
 year=`date +"%Y"`
 yearmonth=`date +"%Y%m"`
 discogsurlprefix="http://discogs-data.s3-us-west-2.amazonaws.com/data/""$year""/"
 
 ##### 2
-# Copy last month's data to old_data dir and remove from current dir
+# - Copy yesterday's data to old_data dir (archive) and remove from downloads dir
 
 dl=/home/downloads/
 olddl=/home/old_downloads/
 
 echo "Moving existing xml files to backup dir"
-find $dl -name \*.xml -print -exec mv {} "$olddl" \;
+find $dl -name \*.xml -print -exec mv {} $olddl \;
 
 ##### 3
-# Curl and gzip decompress each new file
-#### curl fileurl | gunzip -d > test.xml
+# - Curl and gzip decompress each new file
+# - Pipe gzip output to stdout then to file in case of unexpected EOF
+# curl fileurl | gzip -dc > test.xml
 
 for type in artists labels masters releases; \
 	do \
         infile="discogs_"$yearmonth"01_"$type".xml.gz"
         outfile=$dl"discogs_"$yearmonth"01_"$type".xml"
 	    echo "Curling and Gzipping file: " $infile " to " $outfile ;
-	    curl $discogsurlprefix$infile | gzip -d > $outfile; \
+	    curl $discogsurlprefix$infile | gzip -dc > $outfile ; \
 	done
