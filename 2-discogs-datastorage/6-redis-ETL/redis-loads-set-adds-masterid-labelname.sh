24139ddbdd8befa5258e71194287499b038b68b5
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-10 17:28:24 +0000

    master_id in releases collection

--- 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-masterid-labelname.sh
+++ 2-discogs-datastorage/6-redis-ETL/redis-loads-set-adds-masterid-labelname.sh
@@ -3,7 +3,7 @@
 image='dijksterhuis/redis-database-inserts:modularised-0.1'
 container_name='redis-loads-masterid-labelname'
 container_command='./redis-load-set-adder.py'
-container_args='simple_set releases redis-mastersid-labelname masters_id label_name'
+container_args='simple_set releases redis-mastersid-labelname master_id label_name'
 networks='discogs-redis-site-queries discogs-mongo'
 
 docker run -di \
