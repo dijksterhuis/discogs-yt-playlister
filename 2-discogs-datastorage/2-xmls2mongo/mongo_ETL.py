711f71e59778d67c2c7f673a83c60db5ae384630
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-10 11:20:35 +0000

    added released year month

--- 2-discogs-datastorage/2-xmls2mongo/mongo_ETL.py
+++ 2-discogs-datastorage/2-xmls2mongo/mongo_ETL.py
@@ -141,6 +141,7 @@ def handle_elements(path, element):
 	# ---- Create YEAR, MONTH & DATE entries for release date (releases only)
 	if 'released' in element.keys()
 		element['released-year'], element['released-month'], element['released-dotm'] = element['released'].split('-')
+		element['released-yearmonth'] = element['released-year'] + '-' + element['released-month']
 		element['released-date'] = element.pop('released')
 	
 	elif None not in path[1]:
