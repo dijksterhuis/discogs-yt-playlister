1aea24468b97f7651261351847e14d06cba3ef7c
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-30 10:22:37 +0000

    youtube api docker run logic update

--- 4-apis/2-youtube/docker_run.sh
+++ 4-apis/2-youtube/docker_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+docker stop discogs-youtube-api
+docker rm discogs-youtube-api
 docker run -di --restart=always --net=host -v logs:/logging --name discogs-youtube-api dijksterhuis/discogs-youtube-api:dev
 docker network connect discogs-external-apis discogs-youtube-api
