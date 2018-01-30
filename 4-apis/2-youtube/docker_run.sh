9744678d961e87e77e96091d132c653ebd156a3a
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-30 10:19:22 +0000

    update youtube api docker image tag

--- 4-apis/2-youtube/docker_run.sh
+++ 4-apis/2-youtube/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -di --restart=always --net=host -v logs:/logging --name discogs-youtube-api discogs-youtube-api:dev
+docker run -di --restart=always --net=host -v logs:/logging --name discogs-youtube-api dijksterhuis/discogs-youtube-api:dev
 docker network connect discogs-get-apis discogs-youtube-api
