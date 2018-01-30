8dcda84cf439db376f9ae3e4752f400e31d4b200
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-30 10:21:43 +0000

    update run commands

--- 0-docker-admin/up_networks.sh
+++ 0-docker-admin/up_networks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 docker network create --driver bridge --subnet=172.23.0.0/16 discogs-get-apis
+docker network create --driver bridge --subnet=172.25.0.0/16 discogs-external-apis
 docker network create --driver bridge discogs-mongo
 docker network create --driver bridge discogs-redis-autocomplete
 docker network create --driver bridge discogs-redis-caches
--- 4-apis/2-youtube/docker_run.sh
+++ 4-apis/2-youtube/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker run -di --restart=always --net=host -v logs:/logging --name discogs-youtube-api dijksterhuis/discogs-youtube-api:dev
-docker network connect discogs-get-apis discogs-youtube-api
+docker network connect discogs-external-apis discogs-youtube-api
