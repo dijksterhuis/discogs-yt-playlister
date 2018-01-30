dedfe056b397a90377226d3a0cb3375016a5d2c7
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-30 10:26:57 +0000

    update

--- 4-apis/2-youtube/docker_run.sh
+++ 4-apis/2-youtube/docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker stop discogs-youtube-api
 docker rm discogs-youtube-api
-docker run -di --restart=always --ip=172.25.0.2 -v logs:/logging --name discogs-youtube-api dijksterhuis/discogs-youtube-api:dev
-docker network connect discogs-external-apis discogs-youtube-api
+docker run -di --restart=always -v logs:/logging --name discogs-youtube-api dijksterhuis/discogs-youtube-api:dev
+docker network connect --ip=172.25.0.2 discogs-external-apis discogs-youtube-api
+docker network connect host discogs-youtube-api
