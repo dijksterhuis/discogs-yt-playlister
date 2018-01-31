017ce47c67c2c740562bb8111200461d16a3e1ea
dijksterhuis <mrobeson@dundee.ac.uk>
2018-01-31 00:17:40 +0000

    debug off

--- 4-apis/6-redis-autocomplete/redis_flask_api.py
+++ 4-apis/6-redis-autocomplete/redis_flask_api.py
@@ -59,4 +59,4 @@ def label(args):
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=5000,debug=True)
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=False)
