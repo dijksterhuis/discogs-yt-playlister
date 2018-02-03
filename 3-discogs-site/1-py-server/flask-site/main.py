ea51a77815e96d7f9c6e4e6c16d0eca274fec82b
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-03 00:21:29 +0000

    add back the server params in main.py

--- 3-discogs-site/1-py-server/flask-site/main.py
+++ 3-discogs-site/1-py-server/flask-site/main.py
@@ -372,3 +372,7 @@ def send_to_yt():
                                         , first_vid = video_ids[0] \
                                         , test = playlist_result \
                                 )
+
+if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    app.run(host='0.0.0.0',debug=True)
