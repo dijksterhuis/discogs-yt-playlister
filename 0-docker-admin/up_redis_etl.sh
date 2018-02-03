d1f3ba9bedf5cd2df3447ebf183edc079eafbb9c
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-03 21:13:53 +0000

    use getopts

--- 0-docker-admin/up_redis_etl.sh
+++ 0-docker-admin/up_redis_etl.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-
-function parallel_jobs {
-    if [ $2 -z ] ; \
-    then \
-        total_cpus=$(getconf _NPROCESSORS_ONLN) ; \
-        if [ $total_cpus > 2 ] ; \
-        then cpus=$(($total_cpus-2)) ; \
-        else cpus=1 ; \
-        fi ; \
-    else cpus=$2 ; \
-    fi
+while getopts p:e:r:c option
+do
+    case "${option}"
+        in
+        p) PATH=${OPTARG};;
+        e) EXTENSION=${OPTARG};;
+        r) RUN_COMAND=${OPTARG};;
+        c) CPUS=${OPTARG};;
+    esac
     
-    find $1 -print0 -name \*.sh | xargs -0 --max-args=1 --max-procs=$cpus /bin/bash -c
+    function parallel_jobs {
+        if [ $CPUS -z ] ; \
+        then \
+            TOTAL_CPUS=$(getconf _NPROCESSORS_ONLN) ; \
+            if [ $TOTAL_CPUS > 2 ] ; \
+            then CPUS=$(($total_cpus-2)) ; \
+            else CPUS=1 ; \
+            fi ; \
+        fi
+    
+        find $PATH -print0 -name \*.$EXTENSION | xargs -0 --max-args=1 --max-procs=$cpus $RUN_COMMAND
+    }
 
-}
-
-paths="./2-discogs-datastorage/6-redis-ETL/1-searches/ ./2-discogs-datastorage/6-redis-ETL/2-autocomplete/"
-cwd=$(pwd)
-echo "------------------------------------------------"
-echo " --- Starting redis ETL script -----------------"
-echo "------------------------------------------------"
-cd $HOME"/discogs-yt-playlister/"
-for path in $paths ; do parallel_jobs $path ; done
-cd $cwd
+    paths="./2-discogs-datastorage/6-redis-ETL/1-searches/ ./2-discogs-datastorage/6-redis-ETL/2-autocomplete/"
+    cwd=$(pwd)
+    echo "------------------------------------------------"
+    echo " --- Starting redis ETL script -----------------"
+    echo "------------------------------------------------"
+    cd $HOME"/discogs-yt-playlister/"
+    for pth in $paths ; do parallel_jobs -p $pth -e "sh" -c "/bin/bash -c" ; done
+    cd $cwd
+done
