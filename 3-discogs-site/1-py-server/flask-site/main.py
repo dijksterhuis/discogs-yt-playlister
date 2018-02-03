f240c4ffbd9226db937ae4652a614bd1afd405a5
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-03 23:33:47 +0000

    update port

--- 3-discogs-site/1-py-server/flask-site/main.py
+++ 3-discogs-site/1-py-server/flask-site/main.py
@@ -389,4 +389,4 @@ def send_to_yt():
 
 if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True,port=80)
