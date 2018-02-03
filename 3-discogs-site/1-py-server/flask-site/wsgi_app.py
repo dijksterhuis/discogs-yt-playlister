afa70eb425bbb243efdad20e60a3bcbf8af1315a
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-03 00:35:02 +0000

    app to application

--- 3-discogs-site/1-py-server/flask-site/wsgi_app.py
+++ 3-discogs-site/1-py-server/flask-site/wsgi_app.py
@@ -1,6 +1,6 @@
-from main import app
+from main import app as application
 import os
 
 if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    app.run(host='0.0.0.0',debug=True)
+    application.run(host='0.0.0.0',debug=True)
