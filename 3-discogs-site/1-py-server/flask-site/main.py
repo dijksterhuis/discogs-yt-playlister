b637e03384bc85ca3bb0d79d1f19342b7e10afca
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-04 00:09:42 +0000

    update

--- 3-discogs-site/1-py-server/flask-site/main.py
+++ 3-discogs-site/1-py-server/flask-site/main.py
@@ -188,7 +188,7 @@ def query_builder():
                         for name in NAME_FIELDS \
                     }
         
-        return render_template('/test.html',a = NAME_FIELDS,  b=name, c= names, d = name_ids)
+        return render_template('/test.html',a = NAME_FIELDS,  b= names, c = name_ids)
         
         if sum([len(v) for v in name_ids.values()]) == 0:
             flash('No results found for your text input. Please try another search.','message')
